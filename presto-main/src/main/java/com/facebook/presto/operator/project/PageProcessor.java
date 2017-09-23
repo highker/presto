5473bc19e8a2d87ae2bae962317f3941adbc63a3
James Sun <jamessun@fb.com>
2017-09-23 14:02:33 -0700

    Add yield signals to page projection
    Wire yield signals into project functions. A projection may return empty
    pages if it is forced to yield. The yield is at row processing
    granularity for each column.

--- presto-main/src/main/java/com/facebook/presto/operator/project/PageProcessor.java
+++ presto-main/src/main/java/com/facebook/presto/operator/project/PageProcessor.java
@@ -137,6 +137,7 @@ public class PageProcessor
         // remember if we need to re-use the same batch size if we yield last time
         private boolean forceYieldFinish;
         private int previousBatchSize;
+        private Optional<PageProjectionOutput> pageProjectOutput = Optional.empty();
 
         public PositionsPageProcessorIterator(ConnectorSession session, DriverYieldSignal yieldSignal, Page page, SelectedPositions selectedPositions)
         {
@@ -266,7 +267,17 @@ public class PageProcessor
                     blocks[i] = previouslyComputedResults[i].getRegion(0, batchSize);
                 }
                 else {
-                    previouslyComputedResults[i] = projection.project(session, projection.getInputChannels().getInputChannels(page), positionsBatch);
+                    if (!pageProjectOutput.isPresent()) {
+                        pageProjectOutput = Optional.of(projection.project(session, yieldSignal, projection.getInputChannels().getInputChannels(page), positionsBatch));
+                    }
+                    Optional<Block> block = pageProjectOutput.get().compute();
+                    if (!block.isPresent()) {
+                        forceYieldFinish = true;
+                        previousBatchSize = batchSize;
+                        return Optional.empty();
+                    }
+                    pageProjectOutput = Optional.empty();
+                    previouslyComputedResults[i] = block.get();
                     blocks[i] = previouslyComputedResults[i];
                 }
 
