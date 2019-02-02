0d3f8c881077589be400f6f8d90260cc47ec2572
James Sun <jamessun@fb.com>
2019-02-01 20:35:57 -0800

    Move AST to a separate module
    Separate AST from parser as an individual module. Most dependent modules
    only need AST rather than the parser.

--- presto-main/src/main/java/com/facebook/presto/server/HttpRequestSessionContext.java
+++ presto-main/src/main/java/com/facebook/presto/server/HttpRequestSessionContext.java
@@ -16,7 +16,7 @@ package com.facebook.presto.server;
 import com.facebook.presto.Session.ResourceEstimateBuilder;
 import com.facebook.presto.spi.security.Identity;
 import com.facebook.presto.spi.session.ResourceEstimates;
-import com.facebook.presto.sql.parser.ParsingException;
+import com.facebook.presto.sql.ParsingException;
 import com.facebook.presto.sql.parser.ParsingOptions;
 import com.facebook.presto.sql.parser.SqlParser;
 import com.facebook.presto.transaction.TransactionId;
